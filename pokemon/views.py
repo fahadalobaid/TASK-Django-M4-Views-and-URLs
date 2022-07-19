ff109bc82f5ef747b3e1f8fb7a71645f8a65af9c
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-07-19 19:40:24 +0300

    end first part of task

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,3 +1,10 @@
 from django.http import HttpResponse
 
 from .models import Pokemon
+
+def get_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return HttpResponse(pokemon)
+
+
+
